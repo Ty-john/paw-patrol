b678cac4bf00750dc1a89f57af3b382435da8f28
Ian Lester <h352774@gmail.com>
2025-12-05 21:58:18 +0800

    finalization of file structure

--- config.js
+++ config.js
@@ -5,4 +5,8 @@ const CONFIG = {
 
     EMAILJS_VOLUNTEER_SERVICE_ID: 'service_ok7ywyn',
     EMAILJS_VOLUNTEER_TEMPLATE_ID: 'template_59it80f',
+
+    EMAILJS_ADOPT_PUBLIC_KEY: 'vj1rwfJU6brPi1mu8',
+    EMAILJS_ADOPT_SERVICE_ID: 'service_98h5s9h',
+    EMAILJS_ADOPT_TEMPLATE_ID: 'template_z9mmq7v'
 };
