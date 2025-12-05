b25d46fd0ec5c53dfc2e41daeef9607a9feb18bb
Ian Lester <h352774@gmail.com>
2025-12-05 21:25:58 +0800

    add config file

---
+++ paw_patrol/Frontend/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    EMAILJS_PUBLIC_KEY: 'FesRyNq01iowmZMG9',
+    EMAILJS_SERVICE_ID: 'service_e1jjesq',
+    EMAILJS_TEMPLATE_ID: 'template_n7shmio',
+
+    EMAILJS_VOLUNTEER_SERVICE_ID: 'service_ok7ywyn',
+    EMAILJS_VOLUNTEER_TEMPLATE_ID: 'template_59it80f',
+};
